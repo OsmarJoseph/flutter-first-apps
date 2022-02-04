2c71aea95edd075781368ed607bc6ff1d34ace9b
OsmarJoseph <osmarjoseph2013@hotmail.com>
2022-02-03 22:53:24 -0300

    feat: add dicee press handler to update dicee dynamically

--- 5_dicee/lib/main.dart
+++ 5_dicee/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -8,19 +10,63 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.red,
-            appBar:
-                AppBar(title: const Text('Dicee'), backgroundColor: Colors.red),
-            body: const DicePage()));
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(title: const Text('Dicee'), backgroundColor: Colors.red),
+        body: const DicePage(),
+      ),
+    );
   }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
   const DicePage({Key? key}) : super(key: key);
 
   @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  final diceNumbers = {
+    'left': 1,
+    'right': 1,
+  };
+
+  void Function() updateDice() {
+    return () {
+      setState(() {
+        diceNumbers['left'] = Random().nextInt(6) + 1;
+        diceNumbers['right'] = Random().nextInt(6) + 1;
+      });
+    };
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextButton(
+                onPressed: updateDice(),
+                child: Image.asset("images/dice${diceNumbers['left']}.png"),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextButton(
+                onPressed: updateDice(),
+                child: Image.asset("images/dice${diceNumbers['right']}.png"),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
