7c0b4a05e11af08daaeb40676aa2a4b291ad45a2
OsmarJoseph <osmarjoseph2013@hotmail.com>
2022-05-05 19:16:33 -0300

    update android version

--- 7_quizzler-flutter-challenge/android/app/src/main/java/co/appbrewery/quizzler/MainActivity.java
+++ 7_quizzler-flutter-challenge/android/app/src/main/java/co/appbrewery/quizzler/MainActivity.java
@@ -1,13 +1,20 @@
-package co.appbrewery.quizzler;
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
-  }
-}
+ public class MainActivity extends FlutterActivity {
+     private static final String CHANNEL = "samples.flutter.dev/battery";
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler(
+                    (call, result) -> {
+                        // Your existing code
+                }
+        );
+    }
+ }
--- 7_quizzler-flutter-challenge/lib/main.dart
+++ 7_quizzler-flutter-challenge/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-//TODO: Step 2 - Import the rFlutter_Alert package here.
+import 'package:rflutter_alert/rflutter_alert.dart';
 import 'quiz_brain.dart';
 
 QuizBrain quizBrain = QuizBrain();
@@ -35,13 +35,18 @@ class _QuizPageState extends State<QuizPage> {
     bool correctAnswer = quizBrain.getCorrectAnswer();
 
     setState(() {
-      //TODO: Step 4 - Use IF/ELSE to check if we've reached the end of the quiz. If true, execute Part A, B, C, D.
-      //TODO: Step 4 Part A - show an alert using rFlutter_alert (remember to read the docs for the package!)
-      //HINT! Step 4 Part B is in the quiz_brain.dart
-      //TODO: Step 4 Part C - reset the questionNumber,
-      //TODO: Step 4 Part D - empty out the scoreKeeper.
+      if (quizBrain.isFinished()) {
+        Alert(
+                context: context,
+                title: "Alert",
+                desc: "you have reached the questions maximum.")
+            .show();
 
-      //TODO: Step 5 - If we've not reached the end, ELSE do the answer checking steps below 👇
+        quizBrain.reset();
+        scoreKeeper.clear();
+        return;
+      }
+
       if (userPickedAnswer == correctAnswer) {
         scoreKeeper.add(Icon(
           Icons.check,
